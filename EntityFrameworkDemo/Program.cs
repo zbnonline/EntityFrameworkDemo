7cf5f790ee76f7cce110ec8e74388015a1f81e8a
anıl <nbr-nb@hotmail.com>
2023-07-04 12:34:32 +0300

    Add project files.

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GetAll();
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void GetByCategoryId(int categoryId)
+        {
+
+        }
+    }
+}
